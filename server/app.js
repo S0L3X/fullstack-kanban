764f35329ac6b446b180936165fda0ffbb573fe2
S0L3X <s0l3x1@protonmail.com>
2023-08-29 13:17:12 +0200

    update cors

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ const cors = require('cors')
 const app = express();
 
 app.use(cors({
-  origin: 'https://fullstack-kanban.vercel.app'
+  origin: '*'
 }))
 app.use(logger('dev'));
 app.use(express.json());
